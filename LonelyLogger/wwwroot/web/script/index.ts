8fdff8fba482d13798b0df5fe74c887cd783e98b
Jessie Arr <smorrison1986@gmail.com>
2018-06-15 01:45:02 -0500

    Updating home page when logs come in. Ignoring logs folder.

--- LonelyLogger/wwwroot/web/script/index.ts
+++ LonelyLogger/wwwroot/web/script/index.ts
@@ -1,34 +1,46 @@
-﻿var xmlhttp = new XMLHttpRequest();
+﻿
+function getLogs() {
+    var xmlhttp = new XMLHttpRequest();
 
-xmlhttp.onreadystatechange = function () {
-    if (xmlhttp.readyState == XMLHttpRequest.DONE) {   // XMLHttpRequest.DONE == 4
-        if (xmlhttp.status == 200) {
-            var myDiv = document.getElementById("logTable");
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == XMLHttpRequest.DONE) {   // XMLHttpRequest.DONE == 4
+            if (xmlhttp.status == 200) {
+                var myDiv = document.getElementById("logTable");
+                while (myDiv.hasChildNodes()) {
+                    myDiv.removeChild(myDiv.lastChild);
+                }
 
-            var result = JSON.parse(xmlhttp.responseText);
-            result.forEach(function (value) {
-                var newRow = document.createElement("tr");
-                var dateCell = document.createElement("td");
-                var messageCell = document.createElement("td");
+                var result = JSON.parse(xmlhttp.responseText);
+                result.forEach(function (value) {
+                    var newRow = document.createElement("tr");
+                    var dateCell = document.createElement("td");
+                    var messageCell = document.createElement("td");
 
-                var date = new Date(value.log_time);
-                dateCell.innerText = date.toLocaleString();
-                messageCell.innerText = value.message;
+                    var date = new Date(value.log_time);
+                    dateCell.innerText = date.toLocaleString();
+                    messageCell.innerText = value.message;
 
-                newRow.appendChild(dateCell);
-                newRow.appendChild(messageCell);
+                    newRow.appendChild(dateCell);
+                    newRow.appendChild(messageCell);
 
-                myDiv.appendChild(newRow);
-            });
+                    myDiv.appendChild(newRow);
+                });
+            }
+            else if (xmlhttp.status == 400) {
+                alert('There was an error 400');
+            }
+            else {
+                alert('something else other than 200 was returned');
+            }
         }
-        else if (xmlhttp.status == 400) {
-            alert('There was an error 400');
-        }
-        else {
-            alert('something else other than 200 was returned');
-        }
-    }
-};
+    };
 
-xmlhttp.open("GET", "/api/logs", true);
-xmlhttp.send();
+    xmlhttp.open("GET", "/api/logs", true);
+    xmlhttp.send();
+}
+
+getLogs();
+setInterval(function () {
+    getLogs();
+}, 5000);
+
