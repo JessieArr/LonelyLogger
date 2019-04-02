252a6c0d1bc0e90863f9fd855720d7be1ceddf20
Jessie Arr <smorrison1986@gmail.com>
2019-04-02 18:40:47 -0500

    Improving build scripts.

--- BuildAndPublishLinux.ps1
+++ BuildAndPublishLinux.ps1
@@ -3,6 +3,6 @@ dotnet publish -c release -r linux-x64
 Remove-Item "./LonelyLogger/bin/Release/netcoreapp2.1/linux-x64/publish/logs" -Force -Recurse
 Add-Type -Assembly System.IO.Compression.FileSystem
 $compressionLevel = [System.IO.Compression.CompressionLevel]::Optimal
-$sourcedir = "./LonelyLogger/bin/Release/netcoreapp2.1/linux-x64/publish"
-$zipfilename = "LonelyLogger-linux.zip"
+$sourcedir = "$(Get-Location)/LonelyLogger/bin/Release/netcoreapp2.1/linux-x64/publish"
+$zipfilename = "$(Get-Location)/LonelyLogger-linux.zip"
 [System.IO.Compression.ZipFile]::CreateFromDirectory($sourcedir, $zipfilename, $compressionLevel, $false)
--- BuildAndPublishMac.ps1
+++ BuildAndPublishMac.ps1
@@ -3,6 +3,6 @@ dotnet publish -c release -r osx.10.11-x64
 Remove-Item "./LonelyLogger/bin/Release/netcoreapp2.1/osx.10.11-x64/publish/logs" -Force -Recurse
 Add-Type -Assembly System.IO.Compression.FileSystem
 $compressionLevel = [System.IO.Compression.CompressionLevel]::Optimal
-$sourcedir = "./LonelyLogger/bin/Release/netcoreapp2.1/osx.10.11-x64/publish"
-$zipfilename = "LonelyLogger-mac.zip"
+$sourcedir = "$(Get-Location)/LonelyLogger/bin/Release/netcoreapp2.1/osx.10.11-x64/publish"
+$zipfilename = "$(Get-Location)/LonelyLogger-mac.zip"
 [System.IO.Compression.ZipFile]::CreateFromDirectory($sourcedir, $zipfilename, $compressionLevel, $false)
